d4e2f7773175800422c84356766026df7148e03f
Gustavo <gustavo.lima@smi.srv.br>
2021-06-16 11:52:01 -0300

    Added a function for changing colors and increased the wait time

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var is_blue = false;
+var roles_blinking = false;
 
-client.on('ready', ()=> {
+function changeColoers(role) {
+    if (is_blue) {
+        role.setColor("#ff0000");
+        is_blue = false;
+        console.log('red');
+    } else {
+        role.setColor("#0062ff")
+        is_blue = true;
+        console.log('blue');
+    }
+}
+
+client.on('ready', () => {
     console.log('Client ready!');
 
 });
 
-var roles_blinking = false;
 
 client.on('message', (message) => {
-    if(message.content === "#start" & roles_blinking == false){
+    if (message.content === "#start" & roles_blinking == false) {
         roles_blinking = true;
-        let is_blue = false;
         var role = message.guild.roles.cache.get('role id')
-        setInterval(() => {
-            if(is_blue){
-                role.setColor("#ff0000");
-                is_blue = false;
-                console.log('red');
-            }
-            else {
-                role.setColor( "#0062ff")
-                is_blue = true;
-                console.log('blue');
-            }
-
-        }, 5000);
+        setInterval((changeColoers(role), 60000));
+        changeColoers(role);
     }
 })
 
-client.login("token here").then(r => {
+client.login("token").then(r => {
     console.log("Login successfully!");
 });
